d49f2c6a7c5a12a07e45723862d4032f14587a75
Sabadini, Marcelo <m.sabadini@avanade.com>
2021-04-19 16:51:14 -0300

    Iniciando a barra de progresso do ponto onde parou.

--- fila_processar.py
+++ fila_processar.py
@@ -6,43 +6,46 @@ from fake_headers import Headers
 from random import randint
 from tqdm import tqdm
 
-bar = tqdm()
-
+# Esse cara cria o cabeçalho fake para cada requisição
 header = Headers(
     headers=False
 )
 
-df = pd.read_csv('final.csv')
-
 try:
+    # todas URLS
     fila  = pd.read_csv('fila.csv')
+    # o que já foi consultado
     final = pd.read_csv('final.csv')
+    
+    # Configura a barra de rolagem. Seta o tamanho total (fila) e inicia o contador com o que já foi consultado.
+    bar = tqdm(total=len(fila), initial=len(final))
     
     # join entre fila -> final
     fila = pd.merge(fila, final, on='url', how='outer', indicator=True)
     
-    # separa somente o que esta no lado esquerdo, na fila que ainda nao foi consultado
+    # separa somente o que esta no lado esquerdo, ou seja, na fila e que ainda nao foi consultado
     fila = fila[fila['_merge'] == 'left_only']
-    
-    bar.total = len(fila)
     
     for i, row in fila.iterrows():
         
+        # Atualiza a progress bar
         bar.update(1)
         
         r_final = requests.post(row['url'], headers=header.generate()).json()
         
+        # coloca a URL no dataframe final, pois o JOIN é feito com ela
         r_final['url'] = row['url']
         
-        df = df.append(r_final, ignore_index=True)
+        final = final.append(r_final, ignore_index=True)
         
         # grava no disco, pra poder ficar sempre up to date.
-        df.to_csv('final.csv', index=False)
+        final.to_csv('final.csv', index=False)
         
         sleep(1)
         # sleep(randint(0,2))
         
-        if i % 100 == 0:
+        # A cada 50 requests vamos deixar o servidor descansar
+        if i % 50 == 0:
             sleep(30)
        
 except Exception as e:
