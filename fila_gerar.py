a2a7482e8ebd6be391e66c24ffef89e8e31a8219
Sabadini, Marcelo <m.sabadini@avanade.com>
2021-06-18 10:02:24 -0300

    Mexidas nos tempos e fixando consulta somente para carros.

--- fila_gerar.py
+++ fila_gerar.py
@@ -8,8 +8,9 @@ header = Headers(
     headers=False
 )
 
-referencia   = [270]
-veiculo_tipo = [1, 2, 3]
+host         = 'https://veiculos.fipe.org.br/api/veiculos/'
+referencia   = [271]
+veiculo_tipo = [1] # , 2, 3
 
 fila  = pd.DataFrame()
 i = 0
@@ -20,35 +21,35 @@ for tabela_referencia in referencia:
         
         try:
         
-            url = f"https://veiculos.fipe.org.br/api/veiculos//ConsultarMarcas?codigoTabelaReferencia={tabela_referencia}&codigoTipoVeiculo={tipo}"    
+            url = f"{host}/ConsultarMarcas?codigoTabelaReferencia={tabela_referencia}&codigoTipoVeiculo={tipo}"    
             r_marcas = requests.post(url, headers=header.generate()).json()
             
             for marca in r_marcas:
-                url_modelos = f"https://veiculos.fipe.org.br/api/veiculos//ConsultarModelos?codigoTipoVeiculo={tipo}&codigoTabelaReferencia={tabela_referencia}&codigoMarca={marca['Value']}"
+                url_modelos = f"{host}/ConsultarModelos?codigoTipoVeiculo={tipo}&codigoTabelaReferencia={tabela_referencia}&codigoMarca={marca['Value']}"
                 r_modelos = requests.post(url_modelos, headers=header.generate()).json()
                 
                 for modelo in r_modelos['Modelos']:
                     
-                    url_ano_modelo = f"https://veiculos.fipe.org.br/api/veiculos//ConsultarAnoModelo?codigoTipoVeiculo={tipo}&codigoTabelaReferencia={tabela_referencia}&codigoMarca={marca['Value']}&codigoModelo={modelo['Value']}"
+                    url_ano_modelo = f"{host}/ConsultarAnoModelo?codigoTipoVeiculo={tipo}&codigoTabelaReferencia={tabela_referencia}&codigoMarca={marca['Value']}&codigoModelo={modelo['Value']}"
                     r_ano_modelo = requests.post(url_ano_modelo, headers=header.generate()).json()
                     
                     for completa in r_ano_modelo:
                         
                         i += 1
                         
-                        print('URL ', i)
-                        
-                        ano = completa['Value'].split('-')[0]
+                        ano          = completa['Value'].split('-')[0]
                         combustivel  = completa['Value'].split('-')[1]
-                        url_completa = f"https://veiculos.fipe.org.br/api/veiculos//ConsultarValorComTodosParametros?&codigoTabelaReferencia={tabela_referencia}&codigoMarca={marca['Value']}&codigoModelo={modelo['Value']}&codigoTipoVeiculo={tipo}&anoModelo={ano}&codigoTipoCombustivel={combustivel}&tipoConsulta=tradicional"
+                        url_completa = f"{host}/ConsultarValorComTodosParametros?&codigoTabelaReferencia={tabela_referencia}&codigoMarca={marca['Value']}&codigoModelo={modelo['Value']}&codigoTipoVeiculo={tipo}&anoModelo={ano}&codigoTipoCombustivel={combustivel}&tipoConsulta=tradicional"
+                        
+                        print('URL ', i, ': ', tabela_referencia, marca['Value'], modelo['Value'], tipo, ano, combustivel)
                         
                         fila = fila.append({'url': url_completa}, ignore_index=True)
                         
                 fila.to_csv('fila.csv', index=False)
                 
-                sleep(1)
+                sleep(5)
                 
-            sleep(1)
+            sleep(8)
                     
         except Exception as e:
             print('algum BO', e)
